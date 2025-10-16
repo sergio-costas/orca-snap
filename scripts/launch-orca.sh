125bef4878f350667a5bb5bd48bc311e894d131c
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-10-16 15:40:58 +0200

    Added speech-synthesis

--- scripts/launch-orca.sh
+++ scripts/launch-orca.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
+cd $HOME
 $SNAP/usr/bin/orca --debug
--- scripts/launch-speech-dispatcher.sh
+++ scripts/launch-speech-dispatcher.sh
@@ -6,4 +6,5 @@ if ! [ -f $HOME/.config/speech-dispatcher ]; then
     cp -a $SNAP/etc/speech-dispatcher $HOME/.config/speech-dispatcher
 fi
 
+cd $HOME
 $SNAP/usr/bin/speech-dispatcher -s -t 0 -C $HOME/.config/speech-dispatcher
