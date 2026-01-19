a43169b170efa7a4d3808cc3510eb5d8a77cc970
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2026-01-19 15:01:09 +0100

    Make the speech-dispatcher work as daemon
    Also, ensure that the default configuration works by enabling
    espeak-ng

--- scripts/launch-speech-dispatcher.sh
+++ scripts/launch-speech-dispatcher.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 # Copy the default configuration into the user's HOME folder
 # to allow them to customize it.
-if ! [ -f $HOME/.config/speech-dispatcher ]; then
+if [ ! -d $HOME/.config/speech-dispatcher ]; then
     cp -a $SNAP/etc/speech-dispatcher $HOME/.config/speech-dispatcher
 fi
 
